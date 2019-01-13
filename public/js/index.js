2f45b03022019a33a4dd8d1a97c63f2bd3463096
Kenya <kenya@Andrews-MBP-2.attlocal.net>
2019-01-13 02:01:32 -0600

    added updates

--- public/js/index.js
+++ public/js/index.js
@@ -5,7 +5,7 @@ var c = document.getElementById("3").value * 0.2;
 var grade = a + b + c;
 document.getElementById("ans").innerHTML = grade;
 if (grade >=90){document.getElementById("an").innerHTML="You got an A."} 
-else if (grade >= 80){document.getElementById("an").innerHTML="Yo got a B"} 
+else if (grade >= 80){document.getElementById("an").innerHTML="You got a B"} 
 else if (grade >=70){document.getElementById("an").innerHTML="You got a C"}
 else if (grade >=60){document.getElementById("and").innerHTML="You got a D"}  
 else if (grade <=59){document.getElementById("and").innerHTML="You got a F"}  
@@ -21,5 +21,6 @@ else if (grade <=59){document.getElementById("and").innerHTML="You got a F"}
        document.getElementById("2").value=0;
       document.getElementById("3").value=0;
     }
+
 
 
